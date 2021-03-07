836cce5c01e3ea65048ac64ecd38e45145753a1c
sachinrajgire <ketakiharpale@Ketakis-MacBook-Pro-2.local>
2021-03-07 01:23:02 -0500

    deleted solution

--- frequency_Counter.js
+++ frequency_Counter.js
@@ -8,46 +8,9 @@ let a2= [4,10,2]
 
 function compareT () {
 
-// let res
+    return false
 
-// let a1obj={}
-
-// a1.forEach(i=>{
-//     if(a1obj[i] ===undefined){
-//         a1obj[i]=1
-//     }
-//     else {
-//         a1obj[i]=a1obj[i]+1
-//     }
-// })
-// let a2obj={}
-// a2.forEach(i=>{
-//     if(a2obj[i] ===undefined){
-//         a2obj[i]=1
-//     }
-//     else {
-//         a2obj[i]=a2obj[i]+1
-//     }
-// })
-// if(a1.length !== a2.length || Object.keys(a1obj).length !== Object.keys(a2obj) )
-// {
-//     return false 
-// }
-
-// for (let key in a1obj) {
-//     console.log(a1obj[key],a2obj[key]);
-//     if(a1obj[key] !== a2obj[key]){
-//        res=false
-//        break;
-//     }
-//     else {
-//         res=true
-//     } 
-// }
-
-// return res
-// }
-// console.log(compareT(a1,a2) )
+}
 
 module.exports={
     compareT
