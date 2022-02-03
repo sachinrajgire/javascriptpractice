060a32f2b5ce0817e79c64f08f41a9c66b997c43
sachinrajgire <ketakiharpale@Ketakis-MacBook-Pro-2.local>
2022-02-02 21:54:08 -0500

    a

--- Javascript Fundamentals/jf1.js
+++ Javascript Fundamentals/jf1.js
@@ -6,28 +6,95 @@
 
 // For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
-Which one would you prefer ? 
-if (condition){
-    return "do something"
-}
+// for (i=0 ;i <= 100 ; i++) {
 
-if (condition){
-    return "do something"
-}
+//   if( i%3)
+// }
 
-if (condition){
-    return "do something"
-}
-OR 
-if (condition){
-  return "do something"
-}
+// Which one would you prefer ? 
+// if (condition){
+//     return "do something"
+// }
 
-else if (condition){
-  return "do something"
-}
+// if (condition){
+//     return "do something"
+// }
 
-else (condition){
-  return "do something"
-}
+// if (condition){
+//     return "do something"
+// }
+// OR 
+// if (condition){
+//   return "do something"
+// }
+
+// else if (condition){
+//   return "do something"
+// }
+
+// else (condition){
+//   return "do something"
+// }
+
+// // Explicit Return 
+
+// function abc () {
+
+//   if()
+// }
+
+// Inverting Boolean
+
+
+// console.log(3 == "3");
+
+// forced conversti
+// let sachin
+// let a = 3 - "0"
+// console.log(a)
+// console.log(typeof a)
+
+// let b = "0" - 3
+// console.log(b)
+// console.log(typeof b)
+
+// function version (ver) {
+// console.log(Number(ver));
+// console.log(parseInt(ver));
+// console.log(parseFloat(ver));
+// console.log(+(ver),typeof(+(ver)) );
+// //   if (!isNaN(ver) && ver >= 66) {
+// //     return true;
+// //   }
+
+// // return false;
+
+// }
+// console.log(version("66.6"));
+
+
+// Strict Mode - Errors help you
+// Arrow function Explicit Return
+// When can you ignore return and do you need brackets for arguments
+// function arguments calling with Object and arguments 
+// function -- What if we don't know how many items in array ?
+// Anonymous Function - 
+
+//Difference between function declartion and funciton expression
+// Function Scopes 
+// Arrow function 
+
+//Curried Functions
+// const addNums = a => b => a + b
+// console.log(addNums(10)(20))
+
+// DRY ( Dont repeat ) and Single responsibilty 
+//How can you make this function more dynamic , What if business requirements change
+// that number of pieces from 3 to 5
+//should cleaning the fruit included in cutFruits ? 
+// Separate different Concerns 
+
+// function cutFruits (fruit) {
+//     return fruit *3 
+// }
 
